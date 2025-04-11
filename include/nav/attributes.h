772662f4b1fe822039bef2a6a31b62248bb992f4
Miku AuahDark <lumi.hotaru@gmail.com>
2025-04-11 16:16:43 +0800

    Change Linux symbol visibility and improve header attributes.

--- include/nav/attributes.h
+++ include/nav/attributes.h
@@ -6,15 +6,12 @@
 	(defined(__STDC_VERSION__ ) && __STDC_VERSION__ >= 202311L)
 #	define NAV_NODISCARD [[nodiscard]]
 #	define NAV_DEPRECATED [[deprecated]]
-#elif defined(__STDC_VERSION__ ) && __STDC_VERSION__ >= 202311L
-#	define NAV_NODISCARD [[nodiscard]]
-#	define NAV_DEPRECATED [[deprecated]]
+#elif defined(__GNUC__) || defined(__clang__)
+#	define NAV_NODISCARD __attribute__((warn_unused_result))
+#	define NAV_DEPRECATED __attribute__((deprecated))
 #elif defined(_MSC_VER)
 #	include <sal.h>
 #	define NAV_NODISCARD _Check_return_
-#	define NAV_DEPRECATED __declspec(deprecated)
-#elif defined(__GNUC__)
-#	define NAV_NODISCARD __attribute__((warn_unused_result))
 #	define NAV_DEPRECATED __declspec(deprecated)
 #else
 #	define NAV_NODISCARD
